de5e84e5aa7b94580f88ab14dde06050ad0864f0
annaindistress <annaindistress@gmail.com>
2020-07-22 19:15:27 +0300

    Переносит настройку персонажа в модуль setup

--- js/dialog.js
+++ js/dialog.js
@@ -5,12 +5,6 @@
   var popupOpen = document.querySelector('.setup-open');
   var popupClose = popup.querySelector('.setup-close');
   var popupName = popup.querySelector('.setup-user-name');
-  var popupCoat = popup.querySelector('.wizard-coat');
-  var popupCoatInput = popup.querySelector('input[name="coat-color"]');
-  var popupEyes = popup.querySelector('.wizard-eyes');
-  var popupEyesInput = popup.querySelector('input[name="eyes-color"]');
-  var popupFireball = popup.querySelector('.setup-fireball-wrap');
-  var popupFireballInput = popup.querySelector('input[name="fireball-color"]');
   var popupHandle = popup.querySelector('.upload');
 
   var onPopupEscPress = function (evt) {
@@ -33,16 +27,6 @@
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
-  var changeElementColor = function (element, input, color, isBackgroundColor) {
-    if (isBackgroundColor) {
-      element.style.backgroundColor = color;
-    } else {
-      element.style.fill = color;
-    }
-
-    input.value = color;
-  };
-
   popupOpen.addEventListener('click', function () {
     openPopup();
   });
@@ -60,41 +44,6 @@
   popupClose.addEventListener('keydown', function (evt) {
     if (evt.key === 'Enter') {
       closePopup();
-    }
-  });
-
-  var getRandomData = function (array) {
-    var randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
-  };
-
-  popupCoat.addEventListener('click', function () {
-    changeElementColor(popupCoat, popupCoatInput, getRandomData(window.const.wizardCoatColors));
-  });
-
-  popupCoat.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter') {
-      changeElementColor(popupCoat, popupCoatInput, getRandomData(window.const.wizardCoatColors));
-    }
-  });
-
-  popupEyes.addEventListener('click', function () {
-    changeElementColor(popupEyes, popupEyesInput, getRandomData(window.const.wizardEyesColors));
-  });
-
-  popupEyes.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter') {
-      changeElementColor(popupEyes, popupEyesInput, getRandomData(window.const.wizardEyesColors));
-    }
-  });
-
-  popupFireball.addEventListener('click', function () {
-    changeElementColor(popupFireball, popupFireballInput, getRandomData(window.const.wizardFireballColors), true);
-  });
-
-  popupFireball.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter') {
-      changeElementColor(popupFireball, popupFireballInput, getRandomData(window.const.wizardFireballColors), true);
     }
   });
 
--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,58 @@
 'use strict';
 
 (function () {
+  var popupCoat = window.dialog.popup.querySelector('.wizard-coat');
+  var popupCoatInput = window.dialog.popup.querySelector('input[name="coat-color"]');
+  var popupEyes = window.dialog.popup.querySelector('.wizard-eyes');
+  var popupEyesInput = window.dialog.popup.querySelector('input[name="eyes-color"]');
+  var popupFireball = window.dialog.popup.querySelector('.setup-fireball-wrap');
+  var popupFireballInput = window.dialog.popup.querySelector('input[name="fireball-color"]');
+
+  var getRandomData = function (array) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  };
+
+  var changeElementColor = function (element, input, color, isBackgroundColor) {
+    if (isBackgroundColor) {
+      element.style.backgroundColor = color;
+    } else {
+      element.style.fill = color;
+    }
+
+    input.value = color;
+  };
+
+  popupCoat.addEventListener('click', function () {
+    changeElementColor(popupCoat, popupCoatInput, getRandomData(window.const.wizardCoatColors));
+  });
+
+  popupCoat.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      changeElementColor(popupCoat, popupCoatInput, getRandomData(window.const.wizardCoatColors));
+    }
+  });
+
+  popupEyes.addEventListener('click', function () {
+    changeElementColor(popupEyes, popupEyesInput, getRandomData(window.const.wizardEyesColors));
+  });
+
+  popupEyes.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      changeElementColor(popupEyes, popupEyesInput, getRandomData(window.const.wizardEyesColors));
+    }
+  });
+
+  popupFireball.addEventListener('click', function () {
+    changeElementColor(popupFireball, popupFireballInput, getRandomData(window.const.wizardFireballColors), true);
+  });
+
+  popupFireball.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      changeElementColor(popupFireball, popupFireballInput, getRandomData(window.const.wizardFireballColors), true);
+    }
+  });
+
   var wizards = [];
 
   var onSuccessOccurs = function (data) {
