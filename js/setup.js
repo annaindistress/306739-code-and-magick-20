b64850ccba1d78723bb598d5d353fe093cbddfdb
annaindistress <annaindistress@gmail.com>
2020-07-22 22:26:30 +0300

    Добавляет функцию debounce для исправления дребезга

--- js/setup.js
+++ js/setup.js
@@ -41,15 +41,15 @@
     }));
   };
 
-  window.wizard.onEyesChange = function (color) {
+  window.wizard.onEyesChange = window.debounce(function (color) {
     eyesColor = color;
     updateWizards();
-  };
+  });
 
-  window.wizard.onCoatChange = function (color) {
+  window.wizard.onCoatChange = window.debounce(function (color) {
     coatColor = color;
     updateWizards();
-  };
+  });
 
   var onSuccessOccurs = function (data) {
     wizards = data;
